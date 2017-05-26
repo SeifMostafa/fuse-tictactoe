932a9ed4ad167feda8d05709ddf25b3e7fef26c1
edA-qa on MBP <edA-qa@disemia.com>
2017-05-26 12:26:07 +0200

    complete game logic

--- game.js
+++ game.js
@@ -1,15 +1,19 @@
 var Observable = require("FuseJS/Observable")
 
-/* The grid will be 3x3 but we just flatten it here into an array of 9. */
-var numRows = 3
-var numCols = 3
-var numCells = numRows * numCols
-
 /* An enum of the cell states */
 var cellState = {
 	None: 0,
 	O: 1,
 	X: 2,
+}
+
+/* An enum of win conditions */
+var winHow = {
+	None: 0,
+	Row: 1,
+	Col: 2,
+	Diagonal: 3,
+	Tie: 4,
 }
 
 /* Records the position and state of the cells in the grid */
@@ -18,55 +22,115 @@ function Cell( x, y ) {
 	this.x = x
 	this.y = y
 	this.state = Observable(cellState.None)
+	this.win = Observable(false)
 	
 	this.reset = function() {
 		_this.state.value = cellState.None
 	}
 }
 
+/* Contains the game state and functons to progress the game */
+var game = new function() {
+	var _game = this
+	this.curTurn = Observable(cellState.O)
+	this.grid = Observable()
+	/* The grid will be 3x3 but we just flatten it here into an array of 9. */
+	this.size = 3
+	this.used = 0
+	this.gameOver = Observable(false)
 
-var curTurn = Observable(cellState.O)
-var grid = Observable()
-
-/* Initialize the game state */
-function initialize() {
-	for (var y=0; y < numRows; ++y ) {
-		for (var x=0; x <numCols ; ++x ) {
-			grid.add( new Cell(x,y) )
+	for (var y=0; y < _game.size; ++y ) {
+		for (var x=0; x < _game.size ; ++x ) {
+			_game.grid.add( new Cell(x,y) )
 		}
 	}
-}
-
-/* Reset the game to the starting state */
-function reset() {
-	for (var i=0; i < grid.length; ++i) {
-		grid.getAt(i).reset()
+	
+	this.reset = function() {
+		for (var i=0; i < game.grid.length; ++i) {
+			_game.grid.getAt(i).reset()
+		}
+		
+		_game.used = 0
+		_game.curTurn.value = cellState.O
+		_game.gameOver.value = false
 	}
 	
-	curTurn = cellState.O
+	this.getCell = function(x,y) {
+		return _game.grid.getAt(x + y * _game.size)
+	}
+	
+	this.cellClicked = function(cell) {
+		if (cell.state.value != cellState.None) {
+			return
+		}
+	
+		cell.state.value = _game.curTurn.value
+		_game.used++
+		_game.endTurn();
+	}
+	
+	this.endTurn = function() {
+		_game.curTurn.value = _game.curTurn.value == cellState.O ? cellState.X : cellState.O
+		_game.checkEnd()
+	}
+	
+	this.checkEnd = function() {
+		var win = _game.checkWin()
+		if (win.how == winHow.None && _game.used == (_game.size * _game.size)) {
+			win = { how: winHow.Tied }
+		}
+		
+		if (win.how != winHow.None) {
+			_game.gameOver.value = true
+		}
+	}
+	
+	this.checkWin = function() {
+		for (var y=0; y < _game.size; ++y) {
+			var state = _game.getCell(0,y).state.value
+			if (state == cellState.None) {
+				continue;
+			}
+			
+			var okay = true
+			for (var x =1; x < _game.size; ++x) {
+				okay = okay && _game.getCell(x,y).state.value == state
+			}
+			if (okay) { 
+				return { how: winHow.Row, y: y, x: 0 }
+			}
+		}
+		
+		for (var x = 0; x < _game.size; ++x) { 
+			var state = _game.getCell(x,0).state.value
+			if (state == cellState.None) {
+				continue;
+			}
+			
+			var okay = true
+			for (var y = 1; y < _game.size; ++y) {
+				okay = okay && _game.getCell(x,y).state.value == state
+			}
+			if (okay) {
+				return { how: winHow.Col, x: x, y: 0 }
+			}
+		}
+		
+		return { how: winHow.None }
+	}
 }
 
-/* The user has clicked on one of the cells */
+
 function cellClicked(args) {
-	var data = args.data
-	if (data.state.value != cellState.None) {
-		return
-	}
-	
-	data.state.value = curTurn.value
-	endTurn();
+	game.cellClicked(args.data)
 }
 
-function endTurn() {
-	curTurn.value = curTurn.value == cellState.O ? cellState.X : cellState.O
+function restart() {
+	game.reset()
 }
-
-initialize()
 
 module.exports = {
-	numRows: numRows,
-	numCols: numCols,
-	grid: grid,
+	game: game,
 	cellClicked: cellClicked,
-	curTurn: curTurn,
+	restart: restart,
 }
